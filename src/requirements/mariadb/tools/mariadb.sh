e7b5839a870100abac881bf5ad82b270c3d6d35d
root <root@debian.inception.com>
2024-05-20 13:08:56 -0500

    update

--- src/requirements/mariadb/tools/mariadb.sh
+++ src/requirements/mariadb/tools/mariadb.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
+#! /bin/bash
 service mysql start
 
-if [ ! -d /var/lib/mysql/$DATA_BASE_NAME ]; then
-	mysql -u $MARIADB_ROOT_USER -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE $DATA_BASE_NAME;"
-	mysql -e "CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
-	mysql -e "GRANT ALL ON $DATA_BASE_NAME.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD' WITH GRANT OPTION;"
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ];
+then
+	mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE;"
+	mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+	mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 	mysql -e "FLUSH PRIVILEGES;"
 fi
 
-mysqladmin -u $MARIADB_ROOT_USER --password=$MARIADB_ROOT_PASSWORD shutdown
+mysqladmin -u ${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} shutdown
 
 mysqld
